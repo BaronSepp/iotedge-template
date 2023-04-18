018e6ba11a8b7e30df8509b3bc5cb4d64f3b180c
YensDeFraeye-private <68504429+YensDeFraeye-private@users.noreply.github.com>
2023-04-18 13:21:56 +0200

    Ydefraeye/refactor twin handler (#1)
    * Refactor TwinHandler: create a method to access Desired Properties and EventHandler.
    
    * Add code documentation for DefaultTwinHandler.
    
    * Refactor of DefaultMethodHandler.
    
    ---------
    
    Co-authored-by: Yens De Fraeye <ydefraeye@inimco.com>

--- IoTEdge.Template/IoT/TwinHandlers/DefaultTwinHandler.cs
+++ IoTEdge.Template/IoT/TwinHandlers/DefaultTwinHandler.cs
@@ -1,6 +1,7 @@
 using Microsoft.Azure.Devices.Shared;
 using Microsoft.Extensions.Logging;
 using Prometheus;
+using System.Text.Json;
 
 namespace IoTEdge.Template.IoT.TwinHandlers;
 
@@ -11,6 +12,12 @@ public sealed class DefaultTwinHandler : ITwinHandler
 {
 	private readonly Counter _twinUpdateCounter;
 	private readonly ILogger<DefaultTwinHandler> _logger;
+	private readonly IDictionary<string, JsonElement> _twin;
+
+	/// <summary>
+	/// EventHandler to subscribe on when the Desired Properties are changed.
+	/// </summary>
+	public event EventHandler TwinUpdated;
 
 	/// <summary>
 	/// Public <see cref="DefaultTwinHandler"/> constructor, parameters resolved through <b>Dependency injection</b>.
@@ -21,12 +28,32 @@ public sealed class DefaultTwinHandler : ITwinHandler
 	{
 		_twinUpdateCounter = Metrics.CreateCounter("twin_updates_received", "Amount of twin updates received");
 		_logger = logger ?? throw new ArgumentNullException(nameof(logger));
-
-		Twin = new Dictionary<string, object>(0);
+		_twin = new Dictionary<string, JsonElement>(0);
+		TwinUpdated = (_, _) => { };
 	}
 
-	/// <inheritdoc cref="ITwinHandler"/>
-	public IReadOnlyDictionary<string, object> Twin { get; private set; }
+	/// <summary>
+	/// Get properties from Desired Properties in the Module Twin.
+	/// </summary>
+	/// <typeparam name="T">The type of the Desired Property.</typeparam>
+	/// <param name="key">The keyname of the Desired Propeprty.</param>
+	/// <returns>The desired property casted to the given Type.</returns>
+	/// <exception cref="NullReferenceException"></exception>
+	public T GetProperty<T>(string key)
+	{
+		if (string.IsNullOrWhiteSpace(key))
+		{
+			throw new ArgumentNullException(nameof(key));
+		}
+
+		if (_twin.TryGetValue(key, out var value) is false)
+		{
+			throw new ArgumentException("Property was not found!", key);
+		}
+
+		return value.Deserialize<T>() ?? throw new NullReferenceException($"Property {key} could not be parsed to type {typeof(T)}!");
+	}
+
 
 	/// <inheritdoc cref="ITwinHandler.OnDesiredPropertiesUpdate"/>
 	public async Task OnDesiredPropertiesUpdate(TwinCollection desiredProperties, object userContext)
@@ -39,6 +66,40 @@ public sealed class DefaultTwinHandler : ITwinHandler
 			await moduleClient.UpdateReportedPropertiesAsync(desiredProperties);
 		}
 
+		// Get the ModuleTwin's Desired Properties
+		GetDesiredProperties(desiredProperties);
+
+		// Invoke event
+		TwinUpdated.Invoke(this, EventArgs.Empty);
+
 		_twinUpdateCounter.Inc();
+	}
+
+
+	/// <summary>
+	/// Get the Desired Properties.
+	/// Adds them as dictionary pair for later retrieval.
+	/// </summary>
+	/// <param name="twinCollection"></param>
+	/// <exception cref="NullReferenceException"></exception>
+	private void GetDesiredProperties(TwinCollection twinCollection)
+	{
+		var jsonString = twinCollection.ToJson();
+
+		if (string.IsNullOrWhiteSpace(jsonString))
+		{
+			throw new NullReferenceException(nameof(jsonString));
+		}
+
+		using var jsonDocument = JsonDocument.Parse(jsonString);
+		var documentRoot = jsonDocument.RootElement;
+
+		_twin.Clear();
+		foreach (var item in documentRoot.EnumerateObject())
+		{
+			_twin.Add(item.Name, item.Value.Clone());
+		}
+
+		_logger.LogDebug("Twin dictionary set: {dict}", JsonSerializer.Serialize(_twin));
 	}
 }
--- README.md
+++ README.md
@@ -24,3 +24,87 @@ dotnet new iotedge -n <Name> -o <OutputPath>
 cd iotedge-template
 dotnet new uninstall .
 ```
+
+## Features
+
+### TwinHandler
+Via the template's TwinHandler, developers are able to access the Desired Properties of the Module Twin.
+The TwinHandler takes care of Desired Property updates and makes them accessible via the GetProperty method.
+
+#### Access a Desired Property
+
+Below snippet can be used to get a Desired Property named 'sqlConnectionString' of type string.
+```c#
+var sqlConnectionString = _twinHandler.GetProperty<string>("sqlConnectionString");
+```
+
+For reference, below Module Twin is used. Note that 'sqlConnectionString' is a Desired Property of type string.
+```JSON
+{
+	"desired": {
+		"sqlConnectionString": "value here",
+		"sqlPassword": "value here",
+		"listOfThings": [
+			"thing1",
+			"thing2"
+		]
+	}
+}
+```
+
+#### Event handler
+An EventHandler is also availabe to subscribe on when the Desired Properties are updated during application runtime.
+Simply subscribe on it and write logic that needs to be executed when the Properties are updated, like restarting a background job for example.
+
+```c#
+_twinHandler.TwinUpdated += async (s, e) => await OnTwinUpdated();
+```
+
+
+### Environment variables
+Environment Variables are used to setup variables before application runtime. For example, the LogLevel can be configured.
+By default, the LogLevel is set on 'Information' in the appsettings.json.
+
+```
+{
+  "Logging": {
+    "LogLevel": {
+      "Default": "Information",
+      "Microsoft": "Warning",
+      "Microsoft.Hosting.Lifetime": "Information"
+    }
+  },
+
+  "ModuleClient": {
+    "UpstreamProtocol": "AmqpTcp"
+  },
+
+  "Metric": {
+    "HostName": "+",
+    "Port": 9600,
+    "Url": "metrics/",
+    "UseHttps": false
+  }
+}
+```
+
+Via the deployment.template.(debug).json, it can be changed to another level.
+```JSON
+        "modules": {
+          "Inimco.Facts.Modules.Sqlv2": {
+            "version": "1.0.0",
+            "type": "docker",
+            "status": "running",
+            "restartPolicy": "always",
+            "settings": {
+              "image": "${MODULEDIR<../Inimco.Facts.Modules.Sqlv2>}",
+              "createOptions": {}
+            },
+            "env": {
+              "Logging:LogLevel:Default": {
+                "value": "Debug"
+              }
+            }
+          }
+        }
+```
